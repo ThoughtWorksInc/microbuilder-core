0b230d8c686cae09778d779cd79e642c03369e99
Cui Liqiang <cui.liqiang@gmail.com>
2015-08-12 20:53:49 +0800

    implement RouteConfiguration

--- src/haxe/com/thoughtworks/restRpc/core/RouteConfiguration.hx
+++ src/haxe/com/thoughtworks/restRpc/core/RouteConfiguration.hx
@@ -1,4 +1,16 @@
 package com.thoughtworks.restRpc.core;
 
 class RouteConfiguration {
+    private var nameToUriTemplate: Map<String, UriTemplate>;
+}
+
+class UriTemplate {
+    private var template:String;
+    public function new(template:String) {
+        this.template = template;
+    }
+
+    public function render(context: Dynamic):String{
+        return new haxe.Template(template).execute(context);
+    }
 }
