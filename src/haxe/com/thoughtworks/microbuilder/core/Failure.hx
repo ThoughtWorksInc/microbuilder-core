429f6a905ebfb267dd2270d71bfb53b79e26805a
Shao Zengwei <zengwei19861029@163.com>
2015-11-04 20:00:30 +0800

    callee exception

--- src/haxe/com/thoughtworks/microbuilder/core/Failure.hx
+++ src/haxe/com/thoughtworks/microbuilder/core/Failure.hx
@@ -3,8 +3,8 @@ package com.thoughtworks.microbuilder.core;
 //TODO:add status code
 //TODO:500 特殊处理一下
 enum Failure {
-    TEXT_APPLICATION_FAILURE(message:String);
-    STRUCTURAL_APPLICATION_FAILURE(failure:Dynamic);
+    TEXT_APPLICATION_FAILURE(message:String, status:Int);
+    STRUCTURAL_APPLICATION_FAILURE(failure:Dynamic, status:Int);
     NATIVE_FAILURE(message:String);
     SERIALIZATION_FAILURE(reason:String);
 }
