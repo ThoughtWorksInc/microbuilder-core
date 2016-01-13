a00d00d227a3481b29e6a612516a384b954d529f
Yang Bo <atryyang@thoughtworks.com>
2016-01-13 17:40:11 +0800

    Remove old comment

--- src/haxe/com/thoughtworks/microbuilder/core/Failure.hx
+++ src/haxe/com/thoughtworks/microbuilder/core/Failure.hx
@@ -1,6 +1,5 @@
 package com.thoughtworks.microbuilder.core;
 
-//TODO:add status code
 enum Failure {
     TEXT_APPLICATION_FAILURE(message:String, status:Int);
     STRUCTURAL_APPLICATION_FAILURE(failure:Dynamic, status:Int);
